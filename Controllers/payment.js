6ece6aaa2cda1f4652a71656caaa522cda51af2c
Utkarsh Patel <utkarshp04@gmail.com>
2024-03-29 18:42:04 +0530

    Update payment.js

--- Controllers/payment.js
+++ Controllers/payment.js
@@ -3,114 +3,115 @@ const Course = require("../Models/Course");
 const { instance } = require("../Config/razorpay");
 const User = require("../Models/User");
 
-exports.createOrder = async(req,res) =>{
-  
-    const {id,course_id} = req.body;
-;
+
+exports.createOrder = async (req, res) => {
+    const { id, course_id } = req.body;
+
+    try {
+        let sum = 0;
+        if (Array.isArray(course_id)) {
+            for (const element of course_id) {
+                const uid = new mongoose.Types.ObjectId(element._id);
+                sum += parseInt(element.price);
+
+                const courseDetails = await Course.findById(element._id);
+
+                if (courseDetails.studentEnrolled.includes(id)) {
+                    return res.status(500).json({
+                        success: false,
+                        message: 'Course already bought by user'
+                    });
+                }
+            }
+        } else {
+            const uid = new mongoose.Types.ObjectId(id);
+            const courseDetails = await Course.findById(course_id);
+
+            if (courseDetails.studentEnrolled.includes(id)) {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Course already bought by user'
+                });
+            }
+
+            sum = parseInt(courseDetails.price);
+        }
+
+        const amount = sum;
+        const currency = "INR";
+
+        const options = {
+            amount: amount * 100,
+            currency,
+            receipt: Math.random(Date.now()).toString(),
+            notes: {
+                userId: id
+            },
+        };
+
+        const paymentResponse = await instance.orders.create(options);
+        res.status(200).json({
+            success: true,
+            options: {
+                name: Array.isArray(course_id) ? 'Courses' : courseDetails.courseName,
+                image: Array.isArray(course_id) ? undefined : courseDetails.thumbnail,
+                orderId: paymentResponse.id,
+                currency: paymentResponse.currency,
+                amount: paymentResponse.amount,
+                key: 'rzp_test_wstLCIqX2bkcId',
+                notes: options.notes
+            }
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+};
+
+
+exports.verifySignature = async (req, res) => {
+
 
     try {
 
-        const uid = new  mongoose.Types.ObjectId(id);
-       // console.log(uid)
 
-        const courseDetails = await Course.findById(course_id);
+        const { courseId, userId } = req.body;
 
-        if(await courseDetails.studentEnrolled.includes(course_id)) {
-            res.status(500).json({
-                success  : false,
-                message  : 'Course already bought by user'
+        if (Array.isArray(courseId)) {
+            courseId.forEach(async (element) => {
+                await User.findByIdAndUpdate({ _id: userId }, { $push: { courses: element._id } });
+                await Course.findByIdAndUpdate({ _id: element._id }, { $push: { studentEnrolled: userId } });
+
+
             })
-        }
-
-       
-       // console.log(courseDetails)
-
-        const amount = courseDetails.price;
-        const currency = "INR";
-
-        const options  = {
-            amount : amount * 100,
-            currency,
-            receipt : Math.random(Date.now()).toString(),
-            notes : {
-                courseId : course_id,
-                userId : id
-            },
-
-            
-        }
-
-        try{
-            const paymentResponse = await instance.orders.create(options);
             res.status(200).json({
-                success  : true,
-                options : {
-                    name : courseDetails.courseName,
-                    image  :courseDetails.thumbnail,
-                    orderId : paymentResponse.id,
-                    currency : paymentResponse.currency,
-                    amount : paymentResponse.amount,
-                    key : 'rzp_test_wstLCIqX2bkcId',
-                    notes : options.notes
-
-                }
-               
+                success: true,
+                message: 'payment made successfully'
             })
-
         }
 
-        catch(err){
-            res.status(500).json({
-                success  : false,
-                message  : err.message
-            })
-            
+        else {
 
-        }
-
-
-
-
-
-    }
-
-    catch(err){
-        res.status(500).json({
-            success  : false,
-            message  : err.message
-        })
-
-    }
-}
-
-exports.verifySignature = async(req,res) => {
- 
-
-    try{
-        
-            console.log('payment is authorized');
-    
-            const {courseId,userId} = req.body;
-    
-            await User.findByIdAndUpdate({_id : userId},{$push:{courses : courseId}});
-            await Course.findByIdAndUpdate({_id : courseId},{$push:{studentEnrolled : userId}});
+            await User.findByIdAndUpdate({ _id: userId }, { $push: { courses: courseId } });
+            await Course.findByIdAndUpdate({ _id: courseId }, { $push: { studentEnrolled: userId } });
             res.status(200).json({
-                success  : true,
-                message  : 'payment made successfully'
+                success: true,
+                message: 'payment made successfully'
             })
-            
-    
-        
+
+        }
 
     }
 
-    catch(err) {
+    catch (err) {
         res.status(500).json({
-            success  : false,
-            message  : err.message
+            success: false,
+            message: err.message
         })
 
     }
 
-    
+
 }
