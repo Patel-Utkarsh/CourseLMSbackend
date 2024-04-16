e86ddd8075e2c0dd800497226261f131f5598341
Utkarsh Patel <utkarshp04@gmail.com>
2024-04-16 19:23:13 +0530

    Update userProfile.js

--- Controllers/userProfile.js
+++ Controllers/userProfile.js
@@ -31,3 +31,11 @@ exports.updateUserProfile = async(req,res) => {
     }
 }
 
+
+exports.cronJob = async(req,res) => {
+    return res.status(200).json({
+        sucsess  :true,
+        message : 'server is working fine '
+    })
+}
+
